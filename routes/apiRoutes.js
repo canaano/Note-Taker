f9726b4544a7632cc7f1643bc2041955306ca72a
Canaan Owens <canaanowens@Canaans-MacBook-Pro-2021.local>
2022-12-15 11:55:30 -0800

    added js logic to get data form database

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,14 @@
 const router = require("express").Router();
+// setup handler for random id's
+const uuid = require("../helpers/uuid");
+// load fs to write files
+const fs = require("fs");
 
 // get notes from the database
 router.get("/", (req, res) => {
-  res.json(`Got your ${req.method} Request`);
+  console.info(`Got your ${req.method} Request`);
+  const dataFromJson = fs.readFileSync("./db/db.json", "utf8");
+  res.json(JSON.parse(dataFromJson));
 });
 
 // adding notes to the database
