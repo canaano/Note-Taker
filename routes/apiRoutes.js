2e8ed4fc8f853c2d10f95b659fedcd4a8a80f1d6
Canaan Owens <canaanowens@Canaans-MacBook-Pro-2021.local>
2022-12-15 14:56:30 -0800

    added apiroutes post logic, still sorting delete functionality. added helpers folder for uuid

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,16 +4,37 @@ const uuid = require("../helpers/uuid");
 // load fs to write files
 const fs = require("fs");
 
-// get notes from the database
+// get old notes from the database
 router.get("/", (req, res) => {
   console.info(`Got your ${req.method} Request`);
   const dataFromJson = fs.readFileSync("./db/db.json", "utf8");
   res.json(JSON.parse(dataFromJson));
 });
 
-// adding notes to the database
-router.post("/", (req, res) => {
-  res.json(`Got your ${req.method} Request`);
-});
+// adding new notes to the database
+router.post("/", function (req, res) {
+  // add notes to the database
+  console.info(`Received your ${req.method} request`);
+  const { title, text } = req.body;
+  if (title && text) {
+    const newNote = { title, text, id: uuid() };
+    const currentNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    currentNotes.push(newNote);
 
+    fs.writeFile("./db/db.json", JSON.stringify(currentNotes), (err) =>
+      err
+        ? console.err(err)
+        : console.log(`Your New Note for ${newNote.title} has been saved`)
+    );
+    const response = {
+      status: "Saved Note",
+      body: newNote,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json("Your note was not Saved");
+  }
+});
 module.exports = router;
